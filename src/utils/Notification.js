4a843f0570656426c76e662970d6ef2f54d96ca5
sanju <sanjusabu066@gmail.com>
2023-08-20 14:53:15 +0530

    e

--- src/utils/Notification.js
+++ src/utils/Notification.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 
 const notify = async (req) => {
 console.log(req.body);
-    // const client = nodemailer.createTransport({
-    //   service: "Gmail",
-    //   auth: {
-    //     user: process.env.sendMail,
-    //     pass: process.env.Sendpassword,
-    //   },
-    // });
-    // const info = await client.sendMail({
-    //   from: "sanjusabu066@gmail.com",
-    //   to: req.body.email,
-    //   subject: "New Journal Published",
-    //   text: `The code for your password change is: ${otp}`,
-    // });
+    const client = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.sendMail,
+        pass: process.env.Sendpassword,
+      },
+    });
+    const info = await client.sendMail({
+      from: "sanjusabu066@gmail.com",
+      to: req.body.email,
+      subject: "New Journal Published",
+      text: `The code for your password change is: ${otp}`,
+    });
 
     // res.json({ status: info, code: otp, email: req.body.email });
   };
